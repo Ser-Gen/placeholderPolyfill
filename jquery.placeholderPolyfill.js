aa8755cd283518a4d1c4c33d7c739cf2bdd36b12
Sergey Vasilev <sergen-vas@yandex.ru>
2013-05-01 23:21:10 +0400

    Версия 1.0
    - Оптимизировалась проверка поддержки заглушек.
    - Добавился обработчик отправки данных формы, теперь будет возвращаться
    пустое значение.
    - Заглушки в полях ввода пароля выводится текстом, меняясь на «точки» с
    момента ввода пароля пользователем (не работает в ИЕ<9)

--- jquery.placeholderPolyfill.js
+++ jquery.placeholderPolyfill.js
@@ -1,108 +1,110 @@
-/*
- * PlaceHolder Polyfill
- * РёР·-Р·Р° РјРµС‚РѕРґР° .on() С‚СЂРµР±СѓРµС‚СЃСЏ jQuery >1.7
+/*!
+ * placeHolder polyfill 1.0
+ * https://github.com/Ser-Gen/placeholderPolyfill
+ * Поддержка плейсхолдеров в старых браузерах
+ * jQuery >1.7
+ * MIT licensed
  *
- * РџРѕРєР°Р·С‹РІР°РµС‚ С‚РµРєСЃС‚РѕРІС‹Рµ Р·Р°РіР»СѓС€РєРё РІ Р±СЂР°СѓР·РµСЂР°С…, РЅРµ РїРѕРґРґРµСЂР¶РёРІР°СЋС‰РёС… РёС… (РР•<10, Р¤Р¤<4, Рћ<11); РЅРµ РёСЃРїРѕР»РЅСЏРµС‚СЃСЏ РІ РїРѕРґРґРµСЂР¶РёРІР°СЋС‰РёС…
- *
- * РїР°СЂР°РјРµС‚СЂС‹:
- * color вЂ” С†РІРµС‚ РїР»РµР№СЃС…РѕР»РґРµСЂР°
- *
- * РІС‹Р·РѕРІ: 
- * $(СЃРµР»РµРєС‚РѕСЂ).placeholderPolyfill();
- *
- * 09.11.2012
+ * Создан Сергеем Васильевым (@Ser_Gen)
  */
-
 (function($) {
-  var defaults = {
-        color : '#a9a9a9'
-      },
-      options;
-   
-  $.fn.placeholderPolyfill = function (params) {
 
-    options = $.extend ({}, defaults, options, params);
-
-    function restoreVal (elem) {
-      var val = elem.attr('placeholder');
-      if (elem.val() === val) {
-        elem.css('color', options.color);
-      } else if (elem.val().replace(/\s+/, '') === '') {
+    // восстанавливаем значение
+    function restoreVal ( elem ) {
+      var val = elem.attr( 'placeholder' );
+      if ( elem.val() === val ) {
+        elem.css( 'color', '#a9a9a9' );
+      } else if ( elem.val().replace( /\s+/, '' ) === '' ) {
         elem.val(val);
-        elem.css('color', options.color);
+        if ( elem.attr('type') == 'password' ) {
+          try {
+            $(elem).attr( 'data-placeholderType', 'password' ).get(0).type = 'text';
+          } catch (e) {}
+        }
+        elem.css( 'color', '#a9a9a9' );
       }
     }
 
-    function resetCursor (elem) {
-      if (elem.val() == elem.attr('placeholder')) {
+    // устанавливаем положение курсора
+    function resetCursor ( elem ) {
+      if ( elem.val() == elem.attr( 'placeholder' ) ) {
         elem = elem[0];
-        if (elem.setSelectionRange) {
+        if ( elem.setSelectionRange ) {
           elem.focus();
-          elem.setSelectionRange(0, 0);
-        } else if (elem.createTextRange) {
+          elem.setSelectionRange( 0, 0 );
+        } else if ( elem.createTextRange ) {
           var range = elem.createTextRange();
-          range.move('character', 0);
+          range.move( 'character', 0 );
           range.select();
         }
       }
     }
 
-    function preventDel (elem) {
-      var val = elem.attr('placeholder');
-      if (elem.val() === val) {
-        resetCursor(elem);
+    // проверяем состояние при вводе текста
+    function preventDel ( elem ) {
+      var val = elem.attr( 'placeholder' );
+      if ( elem.val() === val ) {
+        resetCursor( elem );
       }
       var waitKey = true;
       elem.keydown(function () {
-        if ((waitKey) && (elem.val() === elem.attr('placeholder'))) {
+        if ( waitKey && ( elem.val() === elem.attr( 'placeholder' ) ) ) {
+          if ( elem.attr( 'data-placeholderType' ) == 'password' ) {
+            try {
+              $(elem).get(0).type = 'password';
+            } catch (e) {}
+          }
           elem.val('');
-          elem.css('color','');
+          elem.css( 'color', '' );
           waitKey = false;
         }
         setTimeout(function () {
-          if (!(waitKey) && (elem.val().length === 0)) {
-            restoreVal(elem);
-            resetCursor(elem);
+          if (!waitKey && ( elem.val().length === 0) ) {
+            restoreVal( elem );
+            resetCursor( elem );
             waitKey = true;
           }
         }, 0);
       });
     }
 
-    function inputProcessor (elem) {
-      restoreVal(elem);
-      elem.focus(function () { preventDel(elem); });
-      elem.blur(function () { restoreVal(elem); });
-      elem.click(function () { resetCursor(elem); });
+    // при отправке данных обнуляем
+    function submitting ( elem ) {
+      var val = elem.attr( 'placeholder' );
+      if ( elem.val() === val ) {
+        elem.val('');
+      }
+    }
+
+    // навешиваем обработчики
+    function inputProcessor ( elem ) {
+      restoreVal( elem );
+      elem.on( 'focus', function () { preventDel(elem); } );
+      elem.on( 'blur', function () { restoreVal(elem); } );
+      elem.on( 'click', function () { resetCursor(elem); } );
+      $(elem).parents('form').on( 'submit', function () { submitting(elem); } );
       $(elem).on('paste', function () {
         elem.val(' ');
         elem.select();
-        elem.css('color','');
+        elem.css( 'color','' );
       });
       $(elem).on('cut', function () {
         setTimeout(function () { restoreVal(elem); }, 0);
       });
     }
 
-    // РїСЂРѕРІРµСЂСЏРµРј, РїРѕРґРґРµСЂР¶РёРІР°СЋС‚СЃСЏ Р»Рё РїР»РµР№СЃС…РѕР»РґРµСЂС‹ Р±СЂР°СѓР·РµСЂРѕРј?
-    var placeholderTestInput = document.createElement("input"),
-        placeholderTestTextarea = document.createElement("textarea");
-    if (typeof placeholderTestInput.placeholder === "undefined") {
-      $(this).each(function () {
-        if ($(this).is('input')) {
-          inputProcessor($(this));
-        };
-      });
-    }
-    if (typeof placeholderTestTextarea.placeholder === "undefined") {
-      $(this).each(function () {
-        if ($(this).is('textarea')) {
-          inputProcessor($(this));
-        }
-      });
-    }
-
-    return this;
+    // проверяем, поддерживаются ли плейсхолдеры браузером
+    $(document).ready(function () {
+      if ( typeof document.createElement( "input" ).placeholder === "undefined" ) {
+        $( 'input' ).each( function () {
+          inputProcessor( $(this) );
+        });
+      }
+      if ( typeof document.createElement( "textarea" ).placeholder === "undefined" ) {
+        $( 'textarea' ).each( function () {
+          inputProcessor( $(this) );
+        });
+      }
+    });
     
-  };
 })(jQuery);
